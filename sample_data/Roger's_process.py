c8757b54081cd2d674a5cbf09ec6065a118bbf4a
ahsokatr <ilaydaecetemir@gmail.com>
2018-06-20 16:01:51 +0200

    new files

---
+++ sample_data/Roger's_process.py
@@ -0,0 +1,268 @@
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv('pamap108_sample10.csv')
+
+
+### HR
+
+result = pd.DataFrame()
+
+result['timestamp'] = df['timestamp']
+result['activity'] = df['activity']
+
+result['hr'] = df['hr'].interpolate()
+
+result.to_csv('pamap_hr.csv')
+
+print df['hr'].isnull().sum()
+
+# ================ HAND ===================
+
+### IMU_HAND TEMPERATURE
+
+result = pd.DataFrame()
+
+result['timestamp'] = df['timestamp']
+result['activity'] = df['activity']
+
+result['temp'] = df['imu_hand_temp'].interpolate()
+
+df['imu_hand_temp'] = df['imu_hand_temp'].fillna(method='ffill')
+print df['imu_hand_temp'].isnull().sum()
+
+result.to_csv('pamap_imuHand_temp.csv')
+
+
+### IMU_HAND ACCELERATION
+
+result = pd.DataFrame()
+
+result['timestamp'] = df['timestamp']
+result['activity'] = df['activity']
+
+result['x'] = df['imu_hand_acc_x_16'].interpolate()
+result['y'] = df['imu_hand_acc_y_16'].interpolate()
+result['z'] = df['imu_hand_acc_z_16'].interpolate()
+
+df['imu_hand_acc_x_16'] = df['imu_hand_acc_x_16'].fillna(method='ffill')
+df['imu_hand_acc_y_16'] = df['imu_hand_acc_y_16'].fillna(method='ffill')
+df['imu_hand_acc_z_16'] = df['imu_hand_acc_z_16'].fillna(method='ffill')
+
+print df['imu_hand_acc_x_16'].isnull().sum()
+print df['imu_hand_acc_y_16'].isnull().sum()
+print df['imu_hand_acc_z_16'].isnull().sum()
+
+result.to_csv('pamap_imuHand_acc.csv')
+
+
+### IMU_HAND GYROSCOPE
+
+result = pd.DataFrame()
+
+result['timestamp'] = df['timestamp']
+result['activity'] = df['activity']
+
+result['x'] = df['imu_hand_gyr_x'].interpolate()
+result['y'] = df['imu_hand_gyr_y'].interpolate()
+result['z'] = df['imu_hand_gyr_z'].interpolate()
+
+df['imu_hand_gyr_x'] = df['imu_hand_gyr_x'].fillna(method='ffill')
+df['imu_hand_gyr_y'] = df['imu_hand_gyr_y'].fillna(method='ffill')
+df['imu_hand_gyr_z'] = df['imu_hand_gyr_z'].fillna(method='ffill')
+
+print df['imu_hand_gyr_x'].isnull().sum()
+print df['imu_hand_gyr_y'].isnull().sum()
+print df['imu_hand_gyr_z'].isnull().sum()
+
+result.to_csv('pamap_imuHand_gyr.csv')
+
+
+### IMU_HAND MAGNETOMETER
+
+result = pd.DataFrame()
+
+result['timestamp'] = df['timestamp']
+result['activity'] = df['activity']
+
+result['x'] = df['imu_hand_mag_x'].interpolate()
+result['y'] = df['imu_hand_mag_y'].interpolate()
+result['z'] = df['imu_hand_mag_z'].interpolate()
+
+df['imu_hand_mag_x'] = df['imu_hand_mag_x'].fillna(method='ffill')
+df['imu_hand_mag_y'] = df['imu_hand_mag_y'].fillna(method='ffill')
+df['imu_hand_mag_z'] = df['imu_hand_mag_z'].fillna(method='ffill')
+
+print df['imu_hand_mag_x'].isnull().sum()
+print df['imu_hand_mag_y'].isnull().sum()
+print df['imu_hand_mag_z'].isnull().sum()
+
+result.to_csv('pamap_imuHand_mag.csv')
+
+
+# ================ CHEST ===================
+
+### imu_chest TEMPERATURE
+
+result = pd.DataFrame()
+
+result['timestamp'] = df['timestamp']
+result['activity'] = df['activity']
+
+result['temp'] = df['imu_chest_temp'].interpolate()
+
+df['imu_chest_temp'] = df['imu_chest_temp'].fillna(method='ffill')
+print df['imu_chest_temp'].isnull().sum()
+
+result.to_csv('pamap_imuChest_temp.csv')
+
+
+### imu_chest ACCELERATION
+
+result = pd.DataFrame()
+
+result['timestamp'] = df['timestamp']
+result['activity'] = df['activity']
+
+result['x'] = df['imu_chest_acc_x_16'].interpolate()
+result['y'] = df['imu_chest_acc_y_16'].interpolate()
+result['z'] = df['imu_chest_acc_z_16'].interpolate()
+
+df['imu_chest_acc_x_16'] = df['imu_chest_acc_x_16'].fillna(method='ffill')
+df['imu_chest_acc_y_16'] = df['imu_chest_acc_y_16'].fillna(method='ffill')
+df['imu_chest_acc_z_16'] = df['imu_chest_acc_z_16'].fillna(method='ffill')
+
+print df['imu_chest_acc_x_16'].isnull().sum()
+print df['imu_chest_acc_y_16'].isnull().sum()
+print df['imu_chest_acc_z_16'].isnull().sum()
+
+result.to_csv('pamap_imuChest_acc.csv')
+
+
+### imu_chest GYROSCOPE
+
+result = pd.DataFrame()
+
+result['timestamp'] = df['timestamp']
+result['activity'] = df['activity']
+
+result['x'] = df['imu_chest_gyr_x'].interpolate()
+result['y'] = df['imu_chest_gyr_y'].interpolate()
+result['z'] = df['imu_chest_gyr_z'].interpolate()
+
+df['imu_chest_gyr_x'] = df['imu_chest_gyr_x'].fillna(method='ffill')
+df['imu_chest_gyr_y'] = df['imu_chest_gyr_y'].fillna(method='ffill')
+df['imu_chest_gyr_z'] = df['imu_chest_gyr_z'].fillna(method='ffill')
+
+print df['imu_chest_gyr_x'].isnull().sum()
+print df['imu_chest_gyr_y'].isnull().sum()
+print df['imu_chest_gyr_z'].isnull().sum()
+
+
+result.to_csv('pamap_imuChest_gyr.csv')
+
+
+### imu_chest MAGNETOMETER
+
+result = pd.DataFrame()
+
+result['timestamp'] = df['timestamp']
+result['activity'] = df['activity']
+
+result['x'] = df['imu_chest_mag_x'].interpolate()
+result['y'] = df['imu_chest_mag_y'].interpolate()
+result['z'] = df['imu_chest_mag_z'].interpolate()
+
+df['imu_chest_mag_x'] = df['imu_chest_mag_x'].fillna(method='ffill')
+df['imu_chest_mag_y'] = df['imu_chest_mag_y'].fillna(method='ffill')
+df['imu_chest_mag_z'] = df['imu_chest_mag_z'].fillna(method='ffill')
+
+print df['imu_chest_mag_x'].isnull().sum()
+print df['imu_chest_mag_y'].isnull().sum()
+print df['imu_chest_mag_z'].isnull().sum()
+
+
+result.to_csv('pamap_imuChest_mag.csv')
+
+
+# =========== ANKLE ===============
+
+
+### imu_ankle TEMPERATURE
+
+result = pd.DataFrame()
+
+result['timestamp'] = df['timestamp']
+result['activity'] = df['activity']
+
+result['temp'] = df['imu_ankle_temp'].interpolate()
+
+df['imu_ankle_temp'] = df['imu_ankle_temp'].fillna(method='ffill')
+print df['imu_ankle_temp'].isnull().sum()
+
+result.to_csv('pamap_imuAnkle_temp.csv')
+
+### imu_ankle ACCELERATION
+
+result = pd.DataFrame()
+
+result['timestamp'] = df['timestamp']
+result['activity'] = df['activity']
+
+result['x'] = df['imu_ankle_acc_x_16'].interpolate()
+result['y'] = df['imu_ankle_acc_y_16'].interpolate()
+result['z'] = df['imu_ankle_acc_z_16'].interpolate()
+
+df['imu_ankle_acc_x_16'] = df['imu_ankle_acc_x_16'].fillna(method='ffill')
+df['imu_ankle_acc_y_16'] = df['imu_ankle_acc_y_16'].fillna(method='ffill')
+df['imu_ankle_acc_z_16'] = df['imu_ankle_acc_z_16'].fillna(method='ffill')
+
+print df['imu_ankle_acc_x_16'].isnull().sum()
+print df['imu_ankle_acc_y_16'].isnull().sum()
+print df['imu_ankle_acc_z_16'].isnull().sum()
+
+result.to_csv('pamap_imuAnkle_acc.csv')
+
+
+### imu_ankle GYROSCOPE
+
+result = pd.DataFrame()
+
+result['timestamp'] = df['timestamp']
+result['activity'] = df['activity']
+
+result['x'] = df['imu_ankle_gyr_x'].interpolate()
+result['y'] = df['imu_ankle_gyr_y'].interpolate()
+result['z'] = df['imu_ankle_gyr_z'].interpolate()
+
+df['imu_ankle_gyr_x'] = df['imu_ankle_gyr_x'].fillna(method='ffill')
+df['imu_ankle_gyr_y'] = df['imu_ankle_gyr_y'].fillna(method='ffill')
+df['imu_ankle_gyr_z'] = df['imu_ankle_gyr_z'].fillna(method='ffill')
+
+print df['imu_ankle_gyr_x'].isnull().sum()
+print df['imu_ankle_gyr_y'].isnull().sum()
+print df['imu_ankle_gyr_z'].isnull().sum()
+
+result.to_csv('pamap_imuAnkle_gyr.csv')
+
+
+### imu_ankle MAGNETOMETER
+
+result = pd.DataFrame()
+
+result['timestamp'] = df['timestamp']
+result['activity'] = df['activity']
+
+result['x'] = df['imu_ankle_mag_x'].interpolate()
+result['y'] = df['imu_ankle_mag_y'].interpolate()
+result['z'] = df['imu_ankle_mag_z'].interpolate()
+
+df['imu_ankle_mag_x'] = df['imu_ankle_mag_x'].fillna(method='ffill')
+df['imu_ankle_mag_y'] = df['imu_ankle_mag_y'].fillna(method='ffill')
+df['imu_ankle_mag_z'] = df['imu_ankle_mag_z'].fillna(method='ffill')
+
+print df['imu_ankle_mag_x'].isnull().sum()
+print df['imu_ankle_mag_y'].isnull().sum()
+print df['imu_ankle_mag_z'].isnull().sum()
+
+result.to_csv('pamap_imuAnkle_mag.csv')
